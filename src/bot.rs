b2dd52333ef26d694f387dad94c0af0538d1266f
VRT-secure <alexshevcov24@gmail.com>
2023-03-26 17:32:35 +0300

    created inline buttons

--- src/bot.rs
+++ src/bot.rs
@@ -1,14 +1,28 @@
+mod parse_xml;
 use teloxide::prelude::*;
+use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup, ReplyMarkup, InlineKeyboardButtonKind};
+use std::error::Error;
 
-async fn my_function() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     pretty_env_logger::init();
-    log::debug!("Starting throw dice bot...");
+    log::info!("Starting currencies bot...");
 
     let bot = Bot::from_env();
+    let yes_button = InlineKeyboardButton::new("Да".to_string(), InlineKeyboardButtonKind::CallbackData("yes".to_string()));
+    let no_button = InlineKeyboardButton::new("Нет".to_string(), InlineKeyboardButtonKind::CallbackData("no".to_string()));
+    
+    // Создаем объект с разметкой для двух кнопок
+    // let markup = InlineKeyboardMarkup::new(vec![vec![yes_button, no_button]]);
+    let keyboard = InlineKeyboardMarkup::default().append_row(vec![yes_button, no_button]);
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        // Отправляем сообщение с кнопками в чат
+        bot.send_message(msg.chat.id, "Debian versions:").reply_markup(keyboard).await?;
         bot.send_message(msg.chat.id, "hello").await?;
         Ok(())
     }).await;
+
+    Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,135 @@
+use std::error::Error;
+use teloxide::{
+    payloads::SendMessageSetters,
+    prelude::*,
+    types::{
+        InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputMessageContent,
+        InputMessageContentText, Me,
+    },
+    utils::command::BotCommands,
+};
 mod parse_xml;
-use parse_xml::*;
+
+
+
+#[derive(BotCommands)]
+#[command(rename_rule = "lowercase", description = "THe bot show currency prices. These commands are supported:")]
+enum Command {
+    #[command(description = "Display this text")]
+    Help,
+    #[command(description = "Start")]
+    Start,
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://www.cbr-xml-daily.ru/daily_eng_utf8.xml";
-    let vec_code = get_char_codes_of_currencies(url)?;
-    let tup_currencie = get_dayli_crb_money("JPY", url)?;
-    println!("{:?}", vec_code);
-    println!("{:?}", tup_currencie);
+async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+    log::info!("Starting buttons bot...");
+
+    let bot = Bot::from_env();
+
+    let handler = dptree::entry()
+        .branch(Update::filter_message().endpoint(message_handler))
+        .branch(Update::filter_callback_query().endpoint(callback_handler))
+        .branch(Update::filter_inline_query().endpoint(inline_query_handler));
+
+    Dispatcher::builder(bot, handler).enable_ctrlc_handler().build().dispatch().await;
     Ok(())
 }
 
+/// Creates a keyboard made by buttons in a big column.
+fn make_keyboard() -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+
+    let url = "https://www.cbr-xml-daily.ru/daily_eng_utf8.xml";
+    let char_codes = match parse_xml::get_char_codes_of_currencies(url) {
+        Ok(v) => {v},
+        Err(e) => {
+            eprintln!("Error downloading XML file: {}", e);
+            vec!["Error".to_string()]
+        },
+    };
+
+    for versions in char_codes.chunks(3) {
+        let row = versions
+            .iter()
+            .map(|version| InlineKeyboardButton::callback(version.to_owned(), version.to_owned()))
+            .collect();
+
+        keyboard.push(row);
+    }
+
+    InlineKeyboardMarkup::new(keyboard)
+}
+
+/// Parse the text wrote on Telegram and check if that text is a valid command
+/// or not, then match the command. If the command is `/start` it writes a
+/// markup with the `InlineKeyboardMarkup`.
+async fn message_handler(
+    bot: Bot,
+    msg: Message,
+    me: Me,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(text) = msg.text() {
+        match BotCommands::parse(text, me.username()) {
+            Ok(Command::Help) => {
+                // Just send the description of all commands.
+                bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
+            }
+            Ok(Command::Start) => {
+                // Create a list of buttons and send them.
+                let keyboard = make_keyboard();
+                bot.send_message(msg.chat.id, "Debian versions:").reply_markup(keyboard).await?;
+            }
+
+            Err(_) => {
+                bot.send_message(msg.chat.id, "Command not found!").await?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn inline_query_handler(
+    bot: Bot,
+    q: InlineQuery,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let choose_debian_version = InlineQueryResultArticle::new(
+        "0",
+        "Chose currency",
+        InputMessageContent::Text(InputMessageContentText::new("Currencyes:")),
+    )
+    .reply_markup(make_keyboard());
+
+    bot.answer_inline_query(q.id, vec![choose_debian_version.into()]).await?;
+
+    Ok(())
+}
+
+/// When it receives a callback from a button it edits the message with all
+/// those buttons writing a text with the selected Debian version.
+///
+/// **IMPORTANT**: do not send privacy-sensitive data this way!!!
+/// Anyone can read data stored in the callback button.
+async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(version) = q.data {
+        let text = format!("You chose: {version}");
+
+        // Tell telegram that we've seen this query, to remove 🕑 icons from the
+        // clients. You could also use `answer_callback_query`'s optional
+        // parameters to tweak what happens on the client side.
+        bot.answer_callback_query(q.id).await?;
+
+        // Edit text of the message to which the buttons were attached
+        if let Some(Message { id, chat, .. }) = q.message {
+            bot.edit_message_text(chat.id, id, text).await?;
+        } else if let Some(id) = q.inline_message_id {
+            bot.edit_message_text_inline(id, text).await?;
+        }
+
+        log::info!("You chose: {}", version);
+    }
+
+    Ok(())
+}
